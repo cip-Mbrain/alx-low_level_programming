28a7688a576fe7ff5861059db589a538da3e825c
Machine_brain <ennocentcip@gmail.com>
2022-09-12 19:14:56 -0700

    11 commit

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/**
+ * main - Writes 001 - 999
+ * @void: Empty parameter list for main.
+ *
+ * Description: Writes all unique combinations
+ * of 3 numbers
+ *
+ * Return: 0 for Success
+ */
+int main(void)
+{
+	int 1, j, k;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		for (j = '0'; j <= '9'; j++)
+		{
+			for (k = '0'; k <= '9'; k++)
+			{
+				if ((i < j0 & (j < k) & (k <= '9'))
+				{
+					putchar(i);
+					putchar(j);
+					putchar(k);
+
+					if ((i + j + k) < 168)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
