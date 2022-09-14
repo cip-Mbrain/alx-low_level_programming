bb9eb58f5dc9a4049006abe24ff6408be06462d6
Machine_brain <ennocentcip@gmail.com>
2022-09-14 09:14:17 -0700

    01

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,18 @@
 #include "main.h"
-
 /**
- * main - print putchar
- * Return: always 0
- */
-
+* main - Call _putchar function to print @c to stdout
+*
+* Return: On success 0 else -1.
+*/
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
+	char a[] = "_putchar";
+	int b = 0;
+
+	do {
+		_putchar(a[b]);
+		b++;
+	} while (b < 8);
 	_putchar('\n');
 	return (0);
 }
