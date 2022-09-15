f922b528663f64f75e47413d6402c60f81bd16ab
Machine_brain <ennocentcip@gmail.com>
2022-09-15 15:17:25 -0700

    02

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul - multiples two numbers
+ * @a: first parameter
+ * @b: second parameter
+ *
+ * Return: result
+ */
+
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+	return (result);
+}
